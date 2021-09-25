23cd8ec120d2ae27b87cbceb7d30f47da299685a
jaystile <46078028+jaystile@users.noreply.github.com>
2021-09-25 13:46:40 -0600

    Solution 3 Checkpoint

--- geometry/src/lib.rs
+++ geometry/src/lib.rs
@@ -1,20 +1,2 @@
-
-/// Point struct represents a point in three dimensional space
-#[allow(dead_code)]
-pub struct Point {
-    x: f64,
-    y: f64,
-    z: f64,
-}
-
-/// Type enum list of supported dimensions
-pub enum Type {
-    D1,
-    D2,
-    D3,
-}
-
-/// Dimensional trait supplies methods for 3D geometry calculations
-pub trait Dimensional {
-    fn dimensions(&self) -> Type;
-}
+pub mod dims;
+pub use dims::{Type,Dimensional};
